e11cd01d4cc33c65d70aba7bbfd8336694a8ac0b
mektigboy <mektigboy@protonmail.com>
2022-07-11 16:26:18 -0500

    Update files

--- src/index.js
+++ src/index.js
@@ -10,19 +10,19 @@ const App = () => {
         author="Alice"
         timeAgo="Today at 03:00 a.m."
         content="Hello!"
-        avatar={faker.image.avatar()}
+        avatar={faker.image.image()}
       />
       <CommentDetail
         author="Bob"
         timeAgo="Today at 01:00 a.m."
         content="Hi!"
-        avatar={faker.image.avatar()}
+        avatar={faker.image.image()}
       />
       <CommentDetail
         author="Carl"
         timeAgo="Today 00:00 a.m."
         content="Hey, guys!"
-        avatar={faker.image.avatar()}
+        avatar={faker.image.image()}
       />
     </div>
   );
