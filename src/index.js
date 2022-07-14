cccee29791249ab3ae6577662d81c3ebd62db743
mektigboy <mektigboy@protonmail.com>
2022-07-13 23:09:25 -0500

    Update files

--- src/index.js
+++ src/index.js
@@ -2,28 +2,35 @@ import React from "react";
 import ReactDOM from "react-dom";
 import faker from "faker";
 import CommentDetail from "./CommentDetail";
+import ApprovalCard from "./ApprovalCard";
 
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetail
-        author="Alice"
-        timeAgo="Today at 03:00 a.m."
-        content="Hello!"
-        avatar={faker.image.image()}
-      />
-      <CommentDetail
-        author="Bob"
-        timeAgo="Today at 01:00 a.m."
-        content="Hi!"
-        avatar={faker.image.image()}
-      />
-      <CommentDetail
-        author="Carl"
-        timeAgo="Today 00:00 a.m."
-        content="Hey, guys!"
-        avatar={faker.image.image()}
-      />
+      <ApprovalCard>
+        <CommentDetail
+          author="Alice"
+          timeAgo="Today at 03:00 a.m."
+          content="Hello!"
+          avatar={faker.image.image()}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="Bob"
+          timeAgo="Today at 01:00 a.m."
+          content="Hi!"
+          avatar={faker.image.image()}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="Carl"
+          timeAgo="Today 00:00 a.m."
+          content="Hey, guys!"
+          avatar={faker.image.image()}
+        />
+      </ApprovalCard>
     </div>
   );
 };
